7a883aa4b26499ffd79a9374c5e0721ea1a9e0c8
tongjh <10255102521@stu.ecnu.edu.cn>
2026-09-22 21:36:57 +0800

    day9：新增 nn_conv2d 卷积层练习与笔记；AGENTS.md 开场用语改为 Master；添加 .vscode 编辑器配置

--- src/nn_conv2d.py
+++ src/nn_conv2d.py
@@ -7,3 +7,41 @@ Day 9 · 卷积层
 """
 
 # TODO: 按计划写你的代码
+
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from torch import nn
+from torch.nn import Conv2d
+from torch.utils.tensorboard import SummaryWriter
+
+
+dataset = torchvision.datasets.CIFAR10("data", train=False, transform=torchvision.transforms.ToTensor(),
+                                       download=True)
+dataloader = DataLoader(dataset, batch_size=64)
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
+
+writer = SummaryWriter("logs/nn_conv2d")
+model = Model()
+
+step = 0
+for data in dataloader:
+    imgs, targets = data
+    output = model(imgs)
+    print(imgs.shape)
+    print(output.shape)
+
+    writer.add_images("input", imgs, step)
+    output = torch.reshape(output, (-1, 3, 30, 30))
+    writer.add_images("output", output, step)
+    step = step + 1
+
+writer.close()
